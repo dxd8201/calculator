25fa3e8bb712c0b7d6a8ba64af530b4c0a67f885
dxd8201 <ddronet17@gmail.com>
2022-12-06 20:44:12 -0600

    Wired all operators work correctly Now outputs correct number when chaining multiple operations Will now take in multiple digit numbers

--- javascript.js
+++ javascript.js
@@ -22,14 +22,74 @@ const output = document.getElementById("output");
 
 
 let displayValue = "";
+let storedNumber = "";
 let storedNumber1 = "";
 let storedNumber2 = "";
 let operator = "";
 let result = 0;
 
+zero.addEventListener('click', () => {
+    displayValue += "0";
+    storeNumber();
+    display();
+})
+
+one.addEventListener('click', () => {
+    displayValue += "1";
+    storeNumber();
+    display();
+})
+
+two.addEventListener('click', () => {
+    displayValue += "2";
+    storeNumber();
+    display();
+})
+
+three.addEventListener('click', () => {
+    displayValue += "3";
+    storeNumber();
+    display();
+})
+
+four.addEventListener('click', () => {
+    displayValue += "4";
+    storeNumber();
+    display();
+})
+
+five.addEventListener('click', () => {
+    displayValue += "5";
+    storeNumber();
+    display();
+})
+
+six.addEventListener('click', () => {
+    displayValue += "6";
+    storeNumber();
+    display();
+})
+
+seven.addEventListener('click', () => {
+    displayValue += "7";
+    storeNumber();
+    display();
+})
+
+eight.addEventListener('click', () => {
+    displayValue += "8";
+    storeNumber();
+    display();
+})
+
+nine.addEventListener('click', () => {
+    displayValue += "9";
+    storeNumber();
+    display();
+})
 
 const add = function(num1, num2) {
-  return parseInt(num1) + parseInt(num2);
+    return parseInt(num1) + parseInt(num2);
 }
 
 const subtract = function(num1, num2) {
@@ -54,15 +114,41 @@ function round(value, precision) {
 //takes a number and stores it in the correct variable to be called later
 const storeNumber = function() {
 
-    const [storedNumber1, storedNumber2] = displayValue.split(/\W{3}(.+)/g);
+        
+        // storedNumber = displayValue.replace(/^\D.+/g, '');
+
+        // if (storedNumber.includes(/\D/g)) {
+        //     console.log("storedNumber held an operator");
+        //     storedNumber = "";
+        // }
+        // const [storedNumber1, storedNumber2] = displayValue.split(/\W{3}(.+)/g);
+        console.log(displayValue);
+        storedArray = displayValue.split(/\W{3}/g);
+        console.log(`StoredArray is ${storedArray}`);
+        
+        //grabs final number of storedArray
+        storedNumber = storedArray.slice(-1)[0];
+        console.log(`Stored Number is ${storedNumber}`);
+        console.log(`Result is ${result}`);
+
+        return storedNumber;
 
     //Store using an array to return multiple values
-    return [storedNumber1, storedNumber2]
+}
+
+const clearNumber = function() {
+    storedNumber = "";
+    console.log(`storedNumber is ${storedNumber}`);
+    return
 }
 
 const operate = function(num1, num2, operator) {
-    
-    if (operator === "+") {
+
+    if (num2 === null) {
+        console.log("num2 is null");
+        console.log(displayValue);
+        return result;
+    } else if (operator === "+") {
        result = add(num1, num2);
        console.log(result);
        return result
@@ -72,6 +158,7 @@ const operate = function(num1, num2, operator) {
         return result
     } else if (operator === "*") {
         result = multiply(num1, num2);
+        console.log("multiply");
         console.log(result);
         return result
     } else if (operator === "/") {
@@ -80,116 +167,173 @@ const operate = function(num1, num2, operator) {
         return result
     } else {
         console.log("Please enter an operator");
-    }
-
-    
+    } 
 }
 
-zero.addEventListener('click', () => {
-    displayValue += "0"
-    display();
-})
-
-one.addEventListener('click', () => {
-    displayValue += "1"
-    display();
-})
-
-two.addEventListener('click', () => {
-    displayValue += "2"
-    display();
-})
-
-three.addEventListener('click', () => {
-    displayValue += "3"
-    display();
-})
-
-four.addEventListener('click', () => {
-    displayValue += "4"
-    display();
-})
-
-five.addEventListener('click', () => {
-    displayValue += "5"
-    display();
-})
-
-six.addEventListener('click', () => {
-    displayValue += "6"
-    display();
-})
-
-seven.addEventListener('click', () => {
-    displayValue += "7"
-    display();
-})
-
-eight.addEventListener('click', () => {
-    displayValue += "8"
-    display();
-})
-
-nine.addEventListener('click', () => {
-    displayValue += "9"
-    display();
-})
+//Should update storeNumber to blank when an operator is pressed
+//need to get operate to run when plus button is clicked, and then clear the storedNumber after
 
 addBtn.addEventListener('click', () => {
-    console.log("Add two numbers");
+    if (operator !== "+" && operator !== "") {
+        operate(result, storedNumber, operator);
+        displayValue += " + ";
+        operator = "+";
+        display();
+        return
+    }
+    
     displayValue += " + ";
     operator = "+";
+
+    if (storedNumber === "") {
+        storedNumber = storedArray.slice(-2)[0];
+        console.log(`2nd to last storedNumber is ${storedNumber}`);
+        display();
+        console.log(result);
+        return console.log("Stored Number is NaN")
+    } else {
+        console.log("Add two numbers");
+        operate(result, storedNumber, operator);
+        display();
+        console.log(result);
+    }
+
     storeNumber();
-    display();
 })
 
 subtractBtn.addEventListener('click', () => {
-    console.log("Subtract two numbers");
+    if (operator !== "-" && operator !== "") {
+        operate(result, storedNumber, operator);
+        displayValue += " - "
+        operator = "-";
+        display();
+        return
+    }
+    
     displayValue += " - "
     operator = "-";
+    console.log("Subtract two numbers");
+    
+    if (result === 0) {
+        result = storedNumber;
+        console.log("Result is the same as storedNumber");
+        return
+    }
+
+    if (storedNumber === "") {
+        storedNumber = storedArray.slice(-2)[0];
+        console.log(`2nd to last storedNumber is ${storedNumber}`);
+        display();
+        console.log(result);
+        return console.log("Stored Number is NaN")
+    } else {
+        console.log("subtract two numbers");
+        operate(result, storedNumber, operator);
+        display();
+        console.log(result);
+    }
+
     storeNumber();
-    display();
+    console.log(`result is ${result}`);
 })
 
 multiplyBtn.addEventListener('click', () => {
-    console.log("Multiply two numbers");
+    if (operator !== "*" && operator !== "") {
+        operate(result, storedNumber, operator);
+        displayValue += " * "
+        operator = "*";
+        display();
+        return
+    }
+    
     displayValue += " * ";
     operator = "*";
+    console.log("Multiply two numbers");
+
+    if (result === 0 && storedNumber.length >= 1) {
+        result = 1;
+    }
+
+    if (storedNumber === "") {
+        storedNumber = storedArray.slice(-2)[0];
+        console.log(`2nd to last storedNumber is ${storedNumber}`);
+        display();
+        console.log(result);
+        return console.log("Stored Number is NaN")
+    } else {
+        console.log("multiply two numbers");
+        operate(result, storedNumber, operator);
+        display();
+        console.log(result);
+    }
+    
+    
     storeNumber();
-    display();
+    console.log(`result is ${result}`);
 })
 
 divisionBtn.addEventListener('click', () => {
-    console.log("Divide two numbers");
+    if (operator !== "/" && operator !== "") {
+        operate(result, storedNumber, operator);
+        displayValue += " / "
+        operator = "/";
+        display();
+        return
+    }
+    
     displayValue += " / ";
     operator = "/";
+    console.log("Divide two numbers");
+    
+    if (result === 0 && storedNumber.length >= 1) {
+        result = storedNumber;
+        return
+    }
+
+    if (storedNumber === "") {
+        storedNumber = storedArray.slice(-2)[0];
+        console.log(`2nd to last storedNumber is ${storedNumber}`);
+        display();
+        console.log(result);
+        return console.log("Stored Number is NaN")
+    } else {
+        console.log("divide two numbers");
+        operate(result, storedNumber, operator);
+        display();
+        console.log(result);
+    }
+
+
     storeNumber();
-    display();
+    console.log(result);
 })
 
 clearBtn.addEventListener('click', () => {
     result = 0;
     displayValue = "";
+    operator = "";
     displayTotal();
 })
 
 decimalBtn.addEventListener('click', () => {
     console.log("Place a decimal");
+    console.log(result);
     displayValue += "."
     console.log(displayValue);
     display();
 })
 
 equalBtn.addEventListener('click', () => {
-    
+    console.log(storedArray);
     //Allows the function values stored in storeNumber to be called 
-    let storedNumbers = storeNumber();
-    storedNumbers;
-    let firstNumber = storedNumbers[0];
-    let secondNumber = storedNumbers[1];
+    // let storedNumbers = storeNumber();
+    // storedNumbers;
+    // let firstNumber = storedNumbers[0];
+    // let secondNumber = storedNumbers[1];
 
-    operate(firstNumber, secondNumber, operator);
+    operate(result, storedNumber, operator);
     displayTotal();
+    storedNumber = "";
     displayValue = "";
 })
 
@@ -199,5 +343,11 @@ const display = function() {
 }
 
 const displayTotal = function() {
-    output.textContent = `${result}`;
+    console.log(result);
+    if (isNaN(result)) {
+        output.textContent = "Error";
+    } else {
+        output.textContent = `${result}`;
+    }
+    
 }
